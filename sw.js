d09017da3d454f55d0c36f379be972484db6eae2
fyskfy4vc8-crypto <fyskfy4vc8@privaterelay.appleid.com>
2026-07-09 22:21:31 -0700

    Initial commit

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'daily-budget-book-v10';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((resp) => {
+        if (event.request.method === 'GET' && resp.ok) {
+          const copy = resp.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
